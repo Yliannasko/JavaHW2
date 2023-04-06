34d668cc51997ac4e9d851fb334852c6013c64b6
Yulia <ylianna_sko@mail.ru>
2023-04-06 11:44:26 +0300

    three

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 10_000; // сколько стоит авиабилет
-        int mile = 20; // сколько стоит 1 миля
-        System.out.println("За купленный билет начисленно бонусов:");
-        System.out.println(ticket / mile);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
